1ace133d7ba8930571f99fc3fffde984287d8bcc
hyebin0523 <bini010523@gmail.com>
2024-02-21 02:27:23 +0900

    add

--- 2588.py
+++ 2588.py
@@ -2,10 +2,31 @@
 int1 = int(input())
 int2 = int(input())
 
-cal1 = int1*(int2%10)
-cal2 = int1*((int2%100)/10)
-cal3 = int1*(int2)/100
+cal1 = int((int2%10)*int1)
+cal2 = int(int((int2%100)/10)*int1)
+cal3 = int(int((int2)/100)*int1)
 print(cal1)
 print(cal2)
 print(cal3)
 print(cal1+cal2*10+cal3*100)
+
+#11382
+A,B,C = input().split()
+A = int(A)
+B = int(B)
+C = int(C)
+
+print(A+B+C)
+
+#10171
+print('\\    /\\')
+print(' )  ( \')')
+print('(  /  )')
+print(' \\(__)|')
+
+#10172
+print('|\\_/|')
+print('|q p|   /}')
+print('( 0 )"""\\')
+print('|"^"`    |')
+print('||_/=\\\\__|')
