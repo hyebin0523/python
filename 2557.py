0a45df7c8db959d6a0462a1a9a39f473105b1157
hyebin0523 <bini010523@gmail.com>
2024-02-21 01:04:29 +0900

    10869

--- 2557.py
+++ 2557.py
@@ -9,4 +9,18 @@ B = int(B)
 if A < 0 or B > 10:
     print('Invalid number')
 else:
-    print(A+B)
+    print(float(A/B))
+
+#10869
+A1,B1 =input().split()
+A1 = int(A1)
+B1 = int(B1)
+
+if A < 1 or B > 10000:
+    print('Invalid number')
+else:
+    print(A1+B1)
+    print(A1-B1)
+    print(A1*B1)
+    print(int(A1/B1))
+    print(A1%B1)
